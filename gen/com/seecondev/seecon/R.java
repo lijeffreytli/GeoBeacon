0820cb2730e000186c9033bb231cf5404ff1e004
Jeffrey Li <lijeffreytli@gmail.com>
2014-10-16 21:23:59 -0500

    created defaults for about/help dialogs in the menu

--- gen/com/seecondev/seecon/R.java
+++ gen/com/seecondev/seecon/R.java
@@ -1097,8 +1097,8 @@ containing a value of this type.
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
-        public static final int button_emergency=0x7f05003e;
-        public static final int button_share_my_location=0x7f05003d;
+        public static final int button_emergency=0x7f05003f;
+        public static final int button_share_my_location=0x7f05003e;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
@@ -1114,6 +1114,7 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
+        public static final int imageView1=0x7f05003c;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int menu_about=0x7f050042;
@@ -1142,8 +1143,7 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int textView1=0x7f05003c;
-        public static final int textView2=0x7f05003f;
+        public static final int text1=0x7f05003d;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
