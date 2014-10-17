d69a6331b6cfad13a5bc9237493d8fb1f81b38a4
Jeffrey Li <lijeffreytli@gmail.com>
2014-10-16 21:12:39 -0500

    added help and about dialogs

--- src/com/seecondev/seecon/MainActivity.java
+++ src/com/seecondev/seecon/MainActivity.java
@@ -1,12 +1,23 @@
 package com.seecondev.seecon;
 
 import android.support.v7.app.ActionBarActivity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
+	
+	static final int DIALOG_ABOUT_ID = 1;
+	static final int DIALOG_HELP_ID = 2;
+	
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +38,63 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    	
+    	switch (item.getItemId()){
+    	case R.id.action_settings:
+    		return true;
+    	case R.id.menu_about:
+    		showDialog(DIALOG_ABOUT_ID);
+    		return true;
+    	case R.id.menu_help:
+    		showDialog(DIALOG_HELP_ID);
+    		return true;
+    	}
+    	return false;
     }
+    
+    @Override
+	protected Dialog onCreateDialog(int id) {
+		Dialog dialog = null;
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+		switch(id) {
+			case DIALOG_ABOUT_ID:
+				dialog = createAboutDialog(builder);
+				break;
+			case DIALOG_HELP_ID:
+				dialog = createHelpDialog(builder);
+				break;
+		}
+
+//		if(dialog == null)
+//			Log.d(TAG, "Uh oh! Dialog is null");
+//		else
+//			Log.d(TAG, "Dialog created: " + id + ", dialog: " + dialog);
+		return dialog;        
+	}
+
+
+	private Dialog createHelpDialog(Builder builder) {
+		// TODO Auto-generated method stub
+				Context context = getApplicationContext();
+				LayoutInflater inflater = (LayoutInflater) context.getSystemService(LAYOUT_INFLATER_SERVICE);
+				View layout = inflater.inflate(R.layout.help_dialog, null); 		
+				builder.setView(layout);
+				builder.setPositiveButton("OK", null);	
+				return builder.create();
+	}
+
+
+	private Dialog createAboutDialog(Builder builder) {
+		// TODO Auto-generated method stub
+		Context context = getApplicationContext();
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(LAYOUT_INFLATER_SERVICE);
+		View layout = inflater.inflate(R.layout.about_dialog, null); 		
+		builder.setView(layout);
+		builder.setPositiveButton("OK", null);	
+		return builder.create();
+	}
+
+
+
 }
