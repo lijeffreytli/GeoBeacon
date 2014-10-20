e6a79ccf02e95e66e10965072f561125e80a5650
Jeffrey Li <lijeffreytli@gmail.com>
2014-10-19 19:33:38 -0500

    added menus to all activities

--- src/com/seecondev/seecon/ShareMyLocation.java
+++ src/com/seecondev/seecon/ShareMyLocation.java
@@ -1,34 +1,97 @@
 package com.seecondev.seecon;
 
 import android.support.v7.app.ActionBarActivity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.AlertDialog.Builder;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class ShareMyLocation extends ActionBarActivity {
+	
+	static final int DIALOG_ABOUT_ID = 1;
+	static final int DIALOG_HELP_ID = 2;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		getActionBar().setDisplayHomeAsUpEnabled(true);
 		setContentView(R.layout.activity_share_my_location);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.share_my_location, menu);
+		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+    	
+    	switch (item.getItemId()){
+    	case R.id.action_settings:
+    		startActivityForResult(new Intent(this, Settings.class),0);  
+    		return true;
+    	case R.id.menu_about:
+    		showDialog(DIALOG_ABOUT_ID);
+    		return true;
+    	case R.id.menu_help:
+    		showDialog(DIALOG_HELP_ID);
+    		return true;
+    	}
+    	return false;
+    }
+    
+    @Override
+	protected Dialog onCreateDialog(int id) {
+		Dialog dialog = null;
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+		switch(id) {
+			case DIALOG_ABOUT_ID:
+				dialog = createAboutDialog(builder);
+				break;
+			case DIALOG_HELP_ID:
+				dialog = createHelpDialog(builder);
+				break;
 		}
-		return super.onOptionsItemSelected(item);
+
+//		if(dialog == null)
+//			Log.d(TAG, "Uh oh! Dialog is null");
+//		else
+//			Log.d(TAG, "Dialog created: " + id + ", dialog: " + dialog);
+		return dialog;        
+	}
+
+
+	private Dialog createHelpDialog(Builder builder) {
+		// TODO Auto-generated method stub
+		Context context = getApplicationContext();
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(LAYOUT_INFLATER_SERVICE);
+		View layout = inflater.inflate(R.layout.help_dialog, null); 		
+		builder.setView(layout);
+		builder.setPositiveButton("OK", null);	
+		return builder.create();
+	}
+
+
+	private Dialog createAboutDialog(Builder builder) {
+		// TODO Auto-generated method stub
+		Context context = getApplicationContext();
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(LAYOUT_INFLATER_SERVICE);
+		View layout = inflater.inflate(R.layout.about_dialog, null); 		
+		builder.setView(layout);
+		builder.setPositiveButton("OK", null);
+		return builder.create();
 	}
 }
