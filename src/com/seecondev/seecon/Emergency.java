f5dd9f3056330669793fb127c063bc6cd3c23f01
Jeffrey Li <lijeffreytli@gmail.com>
2014-10-19 19:53:18 -0500

    made a minor UI change to ShareMyLocation

--- src/com/seecondev/seecon/Emergency.java
+++ src/com/seecondev/seecon/Emergency.java
@@ -32,7 +32,6 @@ public class Emergency extends ActionBarActivity {
 	}
 
 
-	
 	@Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
