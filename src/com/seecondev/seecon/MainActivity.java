8ca6341a8ad2d260febb53a294f39423067a6675
Jeffrey Li <lijeffreytli@gmail.com>
2014-10-20 02:09:10 -0500

    set up the marker and displayed the street address

--- src/com/seecondev/seecon/MainActivity.java
+++ src/com/seecondev/seecon/MainActivity.java
@@ -1,11 +1,17 @@
 package com.seecondev.seecon;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.location.Address;
 import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
@@ -22,6 +28,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
 public class MainActivity extends ActionBarActivity{
@@ -32,7 +39,7 @@ public class MainActivity extends ActionBarActivity{
 	
 	// Google Map
     private GoogleMap googleMap;
-    private LocationClient mLocationClient;
+//    private LocationClient mLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,28 +51,52 @@ public class MainActivity extends ActionBarActivity{
             // Loading map
             initilizeMap();
             googleMap.setMyLocationEnabled(true); // false to disable
+            googleMap.getUiSettings().setMyLocationButtonEnabled(true);
         } catch (Exception e) {
             e.printStackTrace();
         }
         
         // Getting LocationManager object from System Service LOCATION_SERVICE
         LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        
         // Creating a criteria object to retrieve provider
         Criteria criteria = new Criteria();
-        
         // Getting the name of the best provider
         String provider = locationManager.getBestProvider(criteria, true);
-        
         // Getting Current Location
         Location location = locationManager.getLastKnownLocation(provider);
- 
-        LatLng mlocation = null;
-//        if (location == null){
-//        	Log.d(TAG, "location is null");
-//        } else {
-//        	Log.d(TAG, "Success!");
-//        }        
+        // Create variable for LatLng
+        LatLng mlocation = null;  
+        
+        /* This set of code obtains the address, longitude and latitude of a given location */
+        /* Source documentation: http://stackoverflow.com/questions/6922312/get-location-name-from-fetched-coordinates */
+        /* Source documentation: http://wptrafficanalyzer.in/blog/showing-current-location-in-google-maps-with-gps-and-locationmanager-in-android/ */
+        
+        List<String> providerList = locationManager.getAllProviders();
+        if (location != null && provider!=null && providerList.size()>0){
+        	double longitude = location.getLongitude();
+        	double latitude = location.getLatitude();
+        	mlocation = new LatLng(location.getLatitude(), 
+        			location.getLongitude());
+        	Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());   
+        	try {
+        	    List<Address> listAddresses = geocoder.getFromLocation(latitude, longitude, 1);
+        	    if(null!=listAddresses&&listAddresses.size()>0){
+        	        String address = listAddresses.get(0).getAddressLine(0);
+        	        Log.d(TAG, "Current address: " + address);
+        	        Log.d(TAG, "Longitude: " + longitude);
+        	        Log.d(TAG, "Latitude: " + latitude);
+        	        
+        	        // Create a location marker of the user's position
+        	        MarkerOptions marker = new MarkerOptions().position(mlocation).title(address);
+        	     
+        	        // Drop a location marker of the user's position
+        	        googleMap.addMarker(marker);
+        	    }
+        	} catch (IOException e) {
+        	    e.printStackTrace();
+        	}
+        }
+        
         if (location != null) {
         	Log.d(TAG, "Location not null");
         	mlocation = new LatLng(location.getLatitude(), 
@@ -74,6 +105,7 @@ public class MainActivity extends ActionBarActivity{
         } else {
         	Log.d(TAG, "Location is null");
         }   
+        
     }
 
     /**
