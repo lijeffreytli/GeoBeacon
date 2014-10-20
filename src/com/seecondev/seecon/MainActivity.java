4ec6e2d45a9feafcd2e9d26bbc7b9ea890e05cb3
Jeffrey Li <lijeffreytli@gmail.com>
2014-10-19 21:42:43 -0500

    WORKING MAP. YAY

--- src/com/seecondev/seecon/MainActivity.java
+++ src/com/seecondev/seecon/MainActivity.java
@@ -1,31 +1,112 @@
 package com.seecondev.seecon;
 
-import android.support.v7.app.ActionBarActivity;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity{
 	
 	static final int DIALOG_ABOUT_ID = 1;
 	static final int DIALOG_HELP_ID = 2;
+	private static final String TAG = "Tag";
 	
+	// Google Map
+    private GoogleMap googleMap;
+    private LocationClient mLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        
+        try {
+            // Loading map
+            initilizeMap();
+            googleMap.setMyLocationEnabled(true); // false to disable
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+     // Getting LocationManager object from System Service LOCATION_SERVICE
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        
+        // Creating a criteria object to retrieve provider
+        Criteria criteria = new Criteria();
+        
+     // Getting the name of the best provider
+        String provider = locationManager.getBestProvider(criteria, true);
+        
+     // Getting Current Location
+        Location location = locationManager.getLastKnownLocation(provider);
+ 
+        LatLng mlocation = null;
+        if (location == null){
+        	Log.d(TAG, "location is null");
+        } else {
+        	Log.d(TAG, "Success!");
+        }
+        
+        
+//        Location location = googleMap.getMyLocation();
+//       
+//        Log.d(TAG, "HELLOOOO ");
+        
+        if (location != null) {
+        	Log.d(TAG, "NOT NULL ");
+        	mlocation = new LatLng(location.getLatitude(), 
+        			location.getLongitude());
+        	
+        	googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(mlocation, 15));
+        } else {
+        	Log.d(TAG, "ASDFAWEFAW ");
+        }
+        
+        
     }
 
+    /**
+     * function to load map. If map is not created it will create it for you
+     * */
+    private void initilizeMap() {
+        if (googleMap == null) {
+            googleMap = ((MapFragment) getFragmentManager().findFragmentById(
+                    R.id.map)).getMap();
+ 
+            // check if map is created successfully or not
+            if (googleMap == null) {
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+    
+    @Override
+    public void onResume() {
+    	super.onResume();
+        initilizeMap();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
